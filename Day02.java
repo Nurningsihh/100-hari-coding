bc72226d237ac6eca89dd319b9948d9277753051
Nurningsihh <nurningsih3112@gmail.com>
2024-09-03 20:32:26 +0800

    Add files via upload

---
+++ Day02.java
@@ -0,0 +1,13 @@
+
+package day2;
+
+public class  day02{
+
+    public static void main(String[] args) {
+      String nama = "ningsih";
+       
+        System.out.printf(nama);
+        
+    }
+    
+}
