f937020df219f5a697094ea8eef817e5cc667f70
Nurningsihh <nurningsih3112@gmail.com>
2024-09-04 21:35:58 +0800

    Add files via upload

--- Day03.java
+++ Day03.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 3;
-		System.out.print(a);
+		System.out.println("Nurningsih") ;
+		System.out.println("D0222323") ;
+	
 	}
 }
