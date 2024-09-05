0235c731eb9f74de607279ade26ae7216807886f
Nurningsihh <nurningsih3112@gmail.com>
2024-09-05 17:34:23 +0800

    Add files via upload

---
+++ Day04.java
@@ -0,0 +1,15 @@
+package niingg_31;
+
+
+public class Day04 {
+     public static void main(String[] args) {
+    String nama = "ningg";
+        int umur = 21;
+        double tinggi = 1.60;
+        System.out.println("Halo, nama saya " + nama);
+        System.out.print("Umur saya ");
+        System.out.println(umur + " tahun.");
+        System.out.printf("Tinggi badan saya %.2f meter.\n", tinggi);
+     }        
+    
+}
