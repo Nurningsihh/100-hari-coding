771a12d2e22b7ad0ea9e81e2788bdddccb5ae40f
Nurningsihh <nurningsih3112@gmail.com>
2024-09-06 18:08:15 +0800

    Add files via upload

---
+++ Day05.java
@@ -0,0 +1,23 @@
+
+package niingg_31;
+
+
+public class Day05 {
+     public static void main(String[] args) {
+        System.out.print("halo kawan-kawan");
+        System.out.print("apa kabar");
+        /*Mencetak teks atau nilai variabel ke konsol tanpa berpindah ke baris baru*/
+        
+        System.out.println("Nurningsih");
+        System.out.println("Kim Seok Jin");
+        //digunakan, kursor akan berpindah ke awal baris baru.
+        
+        int umur = 21 ;
+        String  nama = "Ningsih";
+        System.out.printf(" nama  :"
+                + "%s,umur : %d%n", nama,umur);
+        
+        //Mencetak teks atau nilai variabel ke konsol dengan format yang lebih fleksibel. Anda bisa menentukan format output menggunakan placeholder seperti %s untuk string, %d untuk integer, %f untuk float, dan lain-lain.
+    }
+    
+}
