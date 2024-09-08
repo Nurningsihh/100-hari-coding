2bcebff6bcdd3b1cef59165c9dc2f330d0232eb5
Nurningsihh <nurningsih3112@gmail.com>
2024-09-08 15:24:52 +0800

    Add files via upload

---
+++ Day07.java
@@ -0,0 +1,22 @@
+
+package niingg_31;
+
+
+public class Day07 {
+    public static void main(String[] args) {//me-inisialisasi variabel di java atau memberikan nilai pada variabel yang telah di deklarasi
+    String nama = "ningsih";
+    String bias = "seokjin";
+    int a = 31;
+    int b = 12;
+    int c = 02;
+    boolean isArmy = true;
+        System.out.println ("nama saya:"+nama+",bias saya:"+bias);
+        System.out.println("angka favorit saya yaitu:"+a+b+c);
+        System.out.println("apakah saya army?"+isArmy);
+        
+    }
+    
+   
+    
+   
+}
