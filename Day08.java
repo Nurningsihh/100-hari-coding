d5e961e1413459e56e214a6e0a54d9160170471a
Nurningsihh <nurningsih3112@gmail.com>
2024-09-09 20:06:23 +0800

    Add files via upload

---
+++ Day08.java
@@ -0,0 +1,29 @@
+
+package niingg_31;
+
+
+public class Day08 {
+    public static void main(String[] args) {
+    String nama = "ningsih";
+           nama = "Nurningsih";
+           System.out.println(nama);
+    String bias = "seokjin";
+           bias = "jimin";
+           System.out.println(bias);
+    int a = 31;
+        a= 9;
+        System.out.println(a);
+    int b = 12;
+        b = 7;
+        System.out.println(b);
+    int c = 02;
+        c = 3;
+        System.out.println(c);
+    boolean isArmy = true;
+            isArmy = false;
+        
+        
+        
+    }
+    
+}
