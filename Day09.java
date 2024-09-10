da36cfeca6161ee4daf12d5a39495dc919c18dc7
Nurningsihh <nurningsih3112@gmail.com>
2024-09-10 21:27:40 +0800

    Add files via upload

---
+++ Day09.java
@@ -0,0 +1,15 @@
+
+package niingg_31;
+
+
+public class Day09 {
+    public static void main(String[] args) {
+        final String nama= "Nurningsih";
+        final int angkaGanjil= 7;
+        
+        System.out.println("nama saya:"+nama);
+        System.out.println("angka ganjil:"+angkaGanjil);
+    }
+    
+    
+}
