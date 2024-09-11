da508ae1a834ad19d301341f03337649efd117fd
Nurningsihh <nurningsih3112@gmail.com>
2024-09-11 17:38:26 +0800

    Add files via upload

---
+++ Day10.java
@@ -0,0 +1,31 @@
+
+package niingg_31;
+
+
+public class Day10 {
+    public static void main(String[] args) {
+   //Tipe data byte adalah tipe data numerik yang hanya dapat menyimpan bilangan bulat dalam rentang -128 hingga 127.
+byte angka1 = 10;
+byte angka2 = -50;
+byte angka3 = 127; 
+byte angka4 = -128; 
+byte angka5 = 0;
+byte angka6 = 32;
+byte angka7 = 64;
+byte angka8 = 96;
+byte angka9 = 110;
+byte angka10 = -100;
+
+        System.out.println(angka1);
+        System.out.println(angka2);
+        System.out.println("nilai maksimum byte"+ angka3);
+        System.out.println("nilai minimum byte"+ angka4);
+        System.out.println(angka5);
+        System.out.println(angka6);
+        System.out.println(angka7);
+        System.out.println(angka8);
+        System.out.println(angka9);
+        System.out.println(angka10);
+    }
+    
+}
