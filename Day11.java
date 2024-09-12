27eefba89eaa5b767f30296400e8b8c772f311ea
Nurningsihh <nurningsih3112@gmail.com>
2024-09-12 22:11:17 +0800

    Add files via upload

---
+++ Day11.java
@@ -0,0 +1,35 @@
+
+package niingg_31;
+
+
+public class Day11 {
+    public static void main(String[] args) {
+        /*
+        Deklarasi dan inisialisasi 11 variabel short
+        Rentang nilai yang dapat disimpan oleh tipe data short adalah dari -32.768 hingga 32.767.
+        */
+        short a = 10;
+        short b = -20000;
+        short c = 32767; // Nilai maksimum short
+        short  d= -32768; // Nilai minimum short
+        short e = 0;
+        short f = 12345;
+        short g= -6543;
+        short h = 10000;
+        short i = -10000;
+        short j = 2000;
+        short k = -2000;
+        
+        System.out.println("nilai a:"+ a);
+        System.out.println("nilai b:"+b);
+        System.out.println("nilai c:"+c);
+        System.out.println("nilai e:"+e);
+        System.out.println("nilai f:"+f);
+        System.out.println("nilai g:"+g);
+        System.out.println("nilai h:"+h);
+        System.out.println("nilai i:"+i);
+        System.out.println("nilai j:"+j);
+        System.out.println("nilai k:"+k);
+
+    }
+}
