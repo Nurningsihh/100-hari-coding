97fcf7139d5fed2470391330cebde4d5ba865302
Nurningsihh <nurningsih3112@gmail.com>
2024-09-13 21:43:15 +0800

    Add files via upload

---
+++ Day12.java
@@ -0,0 +1,42 @@
+
+package niingg_31;
+
+
+public class Day12 {
+    public static void main(String[] args) {
+        //baris pertama
+        int Red ;
+        Red = 7;
+        int Green;
+        Green = 5;
+        int Blue;
+        Blue = 9;
+        int pink;
+        pink = 1;
+        int Purple;
+        Purple = 3;
+        int White;
+        White = 27;
+        //baris kedua
+        int yellow = 8;
+        int silver = 10;
+        int gold = 4;
+        int grey = 100;
+        //baris ketiga
+        int maroon = 13;
+        int voilet = 23;
+        int olive = 5;
+        int peach = 11;
+     System.out.printf("Bilangan Ganjil= %d,%d,%d,%d,%d,%d",Red,Green,Blue,pink,Purple,White);
+     System.out.println("");
+     System.out.println("Bilangan Genap="+yellow+","+silver+","+gold+","+grey);
+        System.out.printf("Bilangan Prima= %d,%d,%d,%d",maroon,voilet,olive,peach);
+     
+     
+        
+       
+        
+                
+    }
+    
+}
