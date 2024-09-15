c6d650ea0d583ba4c9edd62f204a36ac8d33aa53
Nurningsihh <nurningsih3112@gmail.com>
2024-09-15 20:02:00 +0800

    Add files via upload

---
+++ Day14.java
@@ -0,0 +1,53 @@
+
+package niingg_31;
+
+
+public class Day14 {
+    public static void main(String[] args) {
+        // deklarasi 10 variabel
+        long ningsih ;
+        long rana ;
+        long geby ;
+        long dita ;
+        long isfa ;
+        long rahmi ;
+        long madani ;
+        long afika ;
+        long rahmatia ;
+        long dila ;
+        // inisialisasi 10 variabel
+        ningsih = 200000;
+        rana = 300000;
+        geby = 400000;
+        dita = 500000;
+        isfa = 600000;
+        rahmi = 700000;
+        madani = 800000;
+        afika = 900000;
+        dila = 110000;
+        rahmatia = 120000;
+        
+        //inisialisasi 4 variabel tanpa mendeklarasikan terlebih dahulu
+        
+        long seokjin = 5000000;
+        long jimin = 6000000;
+        long jungkook = 7000000;
+        long v = 80000000;
+        
+        //cetak 10 variabel yang sudah di deklarasi dan inisialisasikan menggunakan prinf
+        System.out.printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",ningsih,rana,geby,dita,isfa,rahmi,madani,afika,rahmatia,dila);
+        
+        // cetak 4 variabel
+        System.out.println("");
+        System.out.println("seokjin "+seokjin);
+        System.out.println("jimin "+jimin);
+        System.out.println("jungkook "+jungkook);
+        System.out.println("v "+v);
+                
+                
+                
+         
+        
+    }
+    
+}
