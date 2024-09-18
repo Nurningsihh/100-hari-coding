1c73b5e5801e8934f67cf618ca5149c1e69df1e3
Nurningsihh <nurningsih3112@gmail.com>
2024-09-18 21:16:55 +0800

    Add files via upload

---
+++ Day17.java
@@ -0,0 +1,37 @@
+package niingg_31;
+
+public class Day17 {
+
+    public static void main(String[] args) {
+        char rahmi = 'N';
+        char madani = 'U';
+        char rana = 'R';
+        char afik = 'N';
+        char dila = 'I';
+        char rahmatia = 'N';
+        char geby = 'G';
+        char dita = 'S';
+        char isfa = 'I';
+        char pascol = 'H';
+
+        System.out.println("Nama saya siapaaa?");
+        System.out.printf("%s%s%s%s%s%s%s%s%s%s",rahmi,madani,rana,afik,dila,rahmatia,geby,dita,isfa,pascol);  
+        System.out.println();
+//        System.out.println("TAPI SAYA KURANG WARAS");
+        
+        char r = 'r';
+        char a = 'a';
+        char h = 'h';
+        char m = 'm';
+        char i = 'i';
+        
+        System.out.println("nama teman saya adalah?");
+        System.out.print(r);
+        System.out.print(a);
+        System.out.print(h);
+        System.out.print(m);
+        System.out.println(i);
+//        System.out.println("tapi dia gila");
+    }
+
+}
