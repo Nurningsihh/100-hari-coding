6bb375203c9f88c9abafe5551ed007c8e58d62f8
Nurningsihh <nurningsih3112@gmail.com>
2024-09-27 22:36:54 +0800

    Add files via upload

---
+++ Day26.java
@@ -0,0 +1,59 @@
+
+package niingg_31;
+
+
+public class Day26 {
+   public static void main (String [] args){
+     
+ 
+
+
+Character a = 'h' ;
+Character b = 'a' ;
+Character c = 'l' ;
+Character d = 'o' ;
+
+System.out.print(a);
+System.out.print(b);
+System.out.print(c);
+System.out.print(d);
+
+// mengupdate nilai a,b,c dan d
+
+a = '1';
+b = '2';
+c = '3';
+d = '4';
+       System.out.println("nilai a,b,c dan d");
+System.out.print(a);
+System.out.print(b);
+System.out.print(c);
+System.out.print(d);
+
+Character x = 'a';
+Character y = 'b';
+Character z = 'c';
+       System.out.println("nilai x, y dan z");
+System.out.print(x);
+System.out.print(y);
+System.out.print(z);
+
+Character w = x ;
+x = z;
+z = y;
+y = w ;
+       System.out.println("nilai x,y dan z setelah pertukaran");
+System.out.println(x);
+System.out.println(y);
+System.out.println(z); 
+
+
+          
+
+
+
+
+
+    }
+    
+}
