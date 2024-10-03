13f9e79890709345889ffaea805765650bcd4c3e
Nurningsihh <nurningsih3112@gmail.com>
2024-10-03 22:59:20 +0800

    Add files via upload

---
+++ Day32.java
@@ -0,0 +1,48 @@
+
+package niingg_31;
+
+
+public class Day32 {
+    public static void main(String[] args) {
+         // Mengkonversi int ke String
+        int intValue = 42;
+        String intString = String.valueOf(intValue);
+        System.out.println("Int to String: " + intString);
+
+        // Mengkonversi double ke String
+        double doubleValue = 3.14;
+        String doubleString = String.valueOf(doubleValue);
+        System.out.println("Double to String: " + doubleString);
+
+        // Mengkonversi char ke String
+        char charValue = 'C';
+        String charString = String.valueOf(charValue);
+        System.out.println("Char to String: " + charString);
+
+        // Mengkonversi boolean ke String
+        boolean booleanValue = true;
+        String booleanString = String.valueOf(booleanValue);
+        System.out.println("Boolean to String: " + booleanString);
+
+        // Mengkonversi long ke String
+        long longValue = 1234567890L;
+        String longString = String.valueOf(longValue);
+        System.out.println("Long to String: " + longString);
+
+        // Mengkonversi float ke String
+        float floatValue = 5.75f;
+        String floatString = String.valueOf(floatValue);
+        System.out.println("Float to String: " + floatString);
+
+        // Mengkonversi byte ke String
+        byte byteValue = 10;
+        String byteString = String.valueOf(byteValue);
+        System.out.println("Byte to String: " + byteString);
+
+        // Mengkonversi short ke String
+        short shortValue = 30000;
+        String shortString = String.valueOf(shortValue);
+        System.out.println("Short to String: " + shortString);
+    }
+}
+   
