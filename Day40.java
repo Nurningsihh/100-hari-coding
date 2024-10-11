2b03360d9b4c5b2ea5c0c33e0c5e9e7fe903248f
Nurningsihh <nurningsih3112@gmail.com>
2024-10-11 22:21:40 +0800

    Add files via upload

--- Day40.java
+++ Day40.java
@@ -1,51 +1,37 @@
 
 package niingg_31;
 
+import java.util.Scanner;
+
 public class Day40 {
-    // Method untuk membandingkan dua angka (jumlah buah) dan mencetak hasilnya
-    public static void bandingkanJumlah(int apel, int jeruk) {
-        // Menggunakan operator ternary dan langsung mencetak hasil
-        System.out.println((apel > jeruk) ? ("Apel lebih banyak dari Jeruk") :
-                           (apel < jeruk) ? ("Apel lebih sedikit dari Jeruk") :
-                           ("Apel sama banyak dengan Jeruk"));
-    }
-
     public static void main(String[] args) {
-        // Deklarasi beberapa jumlah buah untuk dibandingkan
-        int apel = 40;
-        int jeruk = 60;
-        int pisang = 25;
+       
+        //deklarasi 5 variabel int untuk di bandingkan
+        int apel = 20;
+        int anggur = 30;
         int mangga = 40;
-        int anggur = 80;
-        int nanas = 15;
+        int pepaya = 50;
+        int pisang = 60;
+        // mencetak hanya menampilkan true false
+        System.out.println(apel> anggur);
+        System.out.println(pisang<pepaya);
+        System.out.println(anggur>mangga);
+        System.out.println(anggur< pepaya);
+        //System.out.println(apel > anggur);
+          
+          Scanner niingg = new Scanner (System.in);
+          System.out.print("masukkan nilai ceri:");
+          int ceri = niingg.nextInt();
+          System.out.print("masukkan nilai kiwi:");
+           int kiwi = niingg.nextInt();
+        System.out.print("ceri lebih besar dari kiwi: ");
+        System.out.println(ceri>kiwi);
+        System.out.print("kiwi lebih besar dari ceri:");
+        System.out.println(kiwi>ceri);
+                
+        
 
-        // Membandingkan jumlah buah satu per satu
-        System.out.println("Perbandingan antara apel dan jeruk:");
-        bandingkanJumlah(apel, jeruk);  
-
-        System.out.println("Perbandingan antara pisang dan apel:");
-        bandingkanJumlah(pisang, apel);  
-
-        System.out.println("Perbandingan antara apel dan mangga:");
-        bandingkanJumlah(apel, mangga);  
-
-        System.out.println("Perbandingan antara anggur dan pisang:");
-        bandingkanJumlah(anggur, pisang);  
-
-        System.out.println("Perbandingan antara nanas dan jeruk:");
-        bandingkanJumlah(nanas, jeruk);  
-
-        System.out.println("Perbandingan antara apel dan anggur:");
-        bandingkanJumlah(apel, anggur);  
-
-        System.out.println("Perbandingan antara pisang dan nanas:");
-        bandingkanJumlah(pisang, nanas);  
-        System.out.println("Perbandingan antara anggur dan mangga:");
-        bandingkanJumlah(anggur, mangga);  
-
-        System.out.println("Perbandingan antara jeruk dan nanas:");
-        bandingkanJumlah(jeruk, nanas); 
-    }
+}
 }
     
 
