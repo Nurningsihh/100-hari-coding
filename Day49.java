756631e83c149eda24deb2c0a3d2563b8e4a7aa2
Nurningsihh <nurningsih3112@gmail.com>
2024-10-20 21:19:53 +0800

    Add files via upload

---
+++ Day49.java
@@ -0,0 +1,55 @@
+
+package niingg_31;
+
+
+public class Day49 {
+    public static void main(String[] args) {
+        int nilaiMatematika = 85;
+        int nilaiBahasaInggris = 75;
+        int nilaiIPA = 92;
+        
+        // Matematika
+        System.out.println("Nilai Matematika: " + nilaiMatematika);
+        if (nilaiMatematika >= 90) {
+            System.out.println("Nilai Matematika: A");
+        } else if (nilaiMatematika >= 80) {
+            System.out.println("Nilai Matematika: B");
+        } else if (nilaiMatematika >= 70) {
+            System.out.println("Nilai Matematika: C");
+        } else if (nilaiMatematika >= 60) {
+            System.out.println("Nilai Matematika: D");
+        } else {
+            System.out.println("Nilai Matematika: E");
+        }
+
+        // Bahasa Inggris
+        System.out.println("\nNilai Bahasa Inggris: " + nilaiBahasaInggris);
+        if (nilaiBahasaInggris >= 90) {
+            System.out.println("Nilai Bahasa Inggris: A");
+        } else if (nilaiBahasaInggris >= 80) {
+            System.out.println("Nilai Bahasa Inggris: B");
+        } else if (nilaiBahasaInggris >= 70) {
+            System.out.println("Nilai Bahasa Inggris: C");
+        } else if (nilaiBahasaInggris >= 60) {
+            System.out.println("Nilai Bahasa Inggris: D");
+        } else {
+            System.out.println("Nilai Bahasa Inggris: E");
+        }
+
+        // IPA
+        System.out.println("\nNilai IPA: " + nilaiIPA);
+        if (nilaiIPA >= 90) {
+            System.out.println("Nilai IPA: A");
+        } else if (nilaiIPA >= 80) {
+            System.out.println("Nilai IPA: B");
+        } else if (nilaiIPA >= 70) {
+            System.out.println("Nilai IPA: C");
+        } else if (nilaiIPA >= 60) {
+            System.out.println("Nilai IPA: D");
+        } else {
+            System.out.println("Nilai IPA: E");
+        }
+    }
+}
+    
+
