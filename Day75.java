bb1d083155f81e097d9c6e210cb0aa156a3f11d8
Nurningsihh <nurningsih3112@gmail.com>
2024-11-15 18:02:26 +0800

    Add files via upload

---
+++ Day75.java
@@ -0,0 +1,33 @@
+
+package niingg_31;
+
+
+import java.util.Scanner;
+
+public class Day75 {
+    public static void main(String[] args) {
+        
+        
+        String kalimat1 = "Halo Dunia!";
+        String kalimat1Lower = kalimat1.toLowerCase();
+        System.out.println("Kalimat asli: " + kalimat1);
+        System.out.println("Kalimat dengan toLowerCase(): " + kalimat1Lower);
+
+ 
+        
+        String kalimat2 = "selamat datang";
+        System.out.println("Kalimat asli: " + kalimat2);
+        System.out.println("Kalimat dengan toLowerCase(): " + kalimat2.toLowerCase());
+
+       
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan kalimat: ");
+        String kalimat3 = scanner.nextLine();
+        System.out.println("Kalimat dengan toLowerCase(): " + kalimat3.toLowerCase());
+
+        scanner.close();
+    }
+}
+
