9f05dcc930f31ba99910c544c611eadf275e511a
Nurningsihh <nurningsih3112@gmail.com>
2024-11-15 18:07:08 +0800

    Add files via upload

--- Day75.java
+++ Day75.java
@@ -8,14 +8,14 @@ public class Day75 {
     public static void main(String[] args) {
         
         
-        String kalimat1 = "Halo Dunia!";
+        String kalimat1 = "Hai Ningsih!";
         String kalimat1Lower = kalimat1.toLowerCase();
         System.out.println("Kalimat asli: " + kalimat1);
         System.out.println("Kalimat dengan toLowerCase(): " + kalimat1Lower);
 
  
         
-        String kalimat2 = "selamat datang";
+        String kalimat2 = "selamat malam";
         System.out.println("Kalimat asli: " + kalimat2);
         System.out.println("Kalimat dengan toLowerCase(): " + kalimat2.toLowerCase());
 
