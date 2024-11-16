31a46e951b0659d59cafb9516733ca1153e571e4
Nurningsihh <nurningsih3112@gmail.com>
2024-11-16 13:08:38 +0800

    Add files via upload

---
+++ Day76.java
@@ -0,0 +1,33 @@
+
+package niingg_31;
+
+
+public class Day76 {
+    public static void main(String[] args) {
+          
+        
+        String nama = "Haii, Ningsih!";
+        String salam = "Selamat siang.";
+        String pesan = "Jangan lupa belajar ya!";
+        
+       
+        
+        String namaUpperCase = nama.toUpperCase();
+        String salamUpperCase = salam.toUpperCase();
+        String pesanUpperCase = pesan.toUpperCase();
+
+        
+        System.out.println("== Contoh Penggunaan toUpperCase() ==");
+        
+        
+        System.out.println("String Asli 1: " + nama);
+        System.out.println("String Huruf Besar 1: " + namaUpperCase);
+        
+        System.out.println("\nString Asli 2: " + salam);
+        System.out.println("String Huruf Besar 2: " + salamUpperCase);
+        
+        System.out.println("\nString Asli 3: " + pesan);
+        System.out.println("String Huruf Besar 3: " + pesanUpperCase);
+    }
+    
+}
